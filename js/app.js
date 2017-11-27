b982cb91ecf24d2e9ae3d3665f80e1c8d90b7626
Dijalma <dijalmacz@gmail.com>
2017-11-27 12:34:41 -0300

    Update images and interaction in images from corrida

--- js/app.js
+++ js/app.js
@@ -1,1 +1,31 @@
-angular.module("app", []);
+$(document).ready(function (){
+
+  var corridas = [
+    {
+      "nome" : "IV Corrida da Independência Cajazeiras-PB",
+      "pasta": "imagens/corridas/indepedencia-cajazeiras-pb",
+      "fotos": [
+        "capa.jpeg",
+        "foto1.jpeg",
+        "foto2.jpeg",
+        "foto3.jpeg",
+        "foto4.jpeg",
+        "foto5.jpeg",
+        "foto6.jpeg",
+        "foto7.jpeg",
+        "foto8.jpeg",
+        "foto9.jpeg"
+      ]
+    }
+  ];
+
+  $("#corrida-indepedencia").hide();
+
+  $("#img-corrida-independencia").click(function () {
+    $("#corrida-indepedencia").fadeIn(300);
+  });
+
+  $(".close").click(function (){
+    $(".slide-horizontal").hide();
+  });
+});
--- js/controllers/corridasControllers.js
+++
@@ -1,8 +0,0 @@
-var app = angular.module("app");
-
-app.controller("CorridasController", ["$scope", "CorridasService", function($scope, CorridasService) {
-
-  CorridasService.retornaCorridas(function (res){
-    $scope.corridas = res;
-  });
-}]);
