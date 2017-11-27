141c2309a584450e4536ada934ac4a51f7890e83
Dijalma <dijalmacz@gmail.com>
2017-11-27 15:07:36 -0300

    show images in full expanded

--- js/app.js
+++ js/app.js
@@ -1,31 +1,48 @@
-$(document).ready(function (){
+$(document).ready(function() {
 
-  var corridas = [
-    {
-      "nome" : "IV Corrida da Independência Cajazeiras-PB",
-      "pasta": "imagens/corridas/indepedencia-cajazeiras-pb",
-      "fotos": [
-        "capa.jpeg",
-        "foto1.jpeg",
-        "foto2.jpeg",
-        "foto3.jpeg",
-        "foto4.jpeg",
-        "foto5.jpeg",
-        "foto6.jpeg",
-        "foto7.jpeg",
-        "foto8.jpeg",
-        "foto9.jpeg"
-      ]
-    }
-  ];
+  var corridas = [{
+    "nome": "IV Corrida da Independência Cajazeiras-PB",
+    "pasta": "imagens/corridas/indepedencia-cajazeiras-pb",
+    "fotos": [
+      "capa.jpeg",
+      "foto1.jpeg",
+      "foto2.jpeg",
+      "foto3.jpeg",
+      "foto4.jpeg",
+      "foto5.jpeg",
+      "foto6.jpeg",
+      "foto7.jpeg",
+      "foto8.jpeg",
+      "foto9.jpeg"
+    ]
+  }];
 
   $("#corrida-indepedencia").hide();
+  $(".image-full").hide();
 
-  $("#img-corrida-independencia").click(function () {
+  $("#img-corrida-independencia").click(function() {
     $("#corrida-indepedencia").fadeIn(300);
   });
 
-  $(".close").click(function (){
+  $(".close").click(function() {
     $(".slide-horizontal").hide();
+  });
+
+  $(".modal-image").click(function() {
+    $(".image-full").hide();
+  });
+
+  $(".backdrop").click(function() {
+    $(".image-full").hide();
+  });
+
+  $(".img-expanded").click(function () {
+
+    var image = $(this);
+    console.log("chamou" + image.attr('src'));
+    $("#image-full").attr("src", image.attr("src"));
+    $("#image-full").attr("alt", image.attr("alt"));
+
+    $(".image-full").fadeIn(300);
   });
 });
