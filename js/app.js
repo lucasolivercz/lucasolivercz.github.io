b35f163400e1bcf2c8918a4efd3f6c05991f5d98
Dijalma <dijalmacz@gmail.com>
2017-11-30 00:46:21 -0300

    Add photos travels

--- js/app.js
+++ js/app.js
@@ -18,14 +18,52 @@ $(document).ready(function() {
   }];
 
   $("#corrida-indepedencia").hide();
+  $("#corrida-sesc-iguatu-ce").hide();
+  $("#corrida-sesc-crato-ce").hide();
+  $("#viagem-crato-ce").hide();
+  $("#viagem-serra-do-vital-pb").hide();
+  $("#viagem-boa-vista-pb").hide();
   $(".image-full").hide();
 
   $("#img-corrida-independencia").click(function() {
-    $("#corrida-indepedencia").fadeIn(300);
+    closeSlideHorizontal();
+    $("#corrida-indepedencia").fadeIn(100);
+    scrollTo("#corrida-indepedencia");
+  });
+
+  $("#img-corrida-sesc-iguatu-ce").click(function() {
+    closeSlideHorizontal();
+    $("#corrida-sesc-iguatu-ce").fadeIn(100);
+    scrollTo("#corrida-sesc-iguatu-ce");
+  });
+
+  $("#img-corrida-sesc-crato-ce").click(function() {
+    closeSlideHorizontal();
+    $("#corrida-sesc-crato-ce").fadeIn(100);
+    scrollTo("#corrida-sesc-crato-ce");
+  });
+
+  $("#img-viagem-crato-ce").click(function() {
+    closeSlideHorizontal();
+    $("#viagem-crato-ce").fadeIn(100);
+    scrollTo("#viagem-crato-ce");
+  });
+
+  $("#img-viagem-boa-vista-pb").click(function() {
+    closeSlideHorizontal();
+    $("#viagem-boa-vista-pb").fadeIn(100);
+    scrollTo("#viagem-boa-vista-pb");
+  });
+
+  $("#img-viagem-serra-do-vital-pb").click(function() {
+    closeSlideHorizontal();
+    $("#viagem-serra-do-vital-pb").fadeIn(100);
+    scrollTo("#viagem-serra-do-vital-pb");
   });
 
   $(".close").click(function() {
-    $(".slide-horizontal").hide();
+    scrollBackDiv();
+    closeSlideHorizontal();
   });
 
   $(".modal-image").click(function() {
@@ -36,13 +74,28 @@ $(document).ready(function() {
     $(".image-full").hide();
   });
 
-  $(".img-expanded").click(function () {
+  $(".img-expanded").click(function() {
 
     var image = $(this);
-    console.log("chamou" + image.attr('src'));
     $("#image-full").attr("src", image.attr("src"));
     $("#image-full").attr("alt", image.attr("alt"));
 
     $(".image-full").fadeIn(300);
   });
 });
+
+function scrollTo(idDiv) {
+  $('html, body').animate({
+    scrollTop: $(idDiv).offset().top - 8
+  }, 1000);
+}
+
+function scrollBackDiv() {
+  $('html, body').animate({
+    scrollTop: $(window).scrollTop() - 400
+  }, 1000);
+}
+
+function closeSlideHorizontal() {
+  $(".slide-horizontal").hide();
+}
